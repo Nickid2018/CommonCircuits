755a1b00b78240f4cb729586ce35fc40dffd2f6b
Nickid2018 <nickid2018@outlook.com>
2023-02-08 02:22:20 +0800

    Initial Commit

---
+++ src/main/java/io/github/nickid2018/commoncircuits/CommonCircuitsMain.java
@@ -0,0 +1,10 @@
+package io.github.nickid2018.commoncircuits;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CommonCircuitsMain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
