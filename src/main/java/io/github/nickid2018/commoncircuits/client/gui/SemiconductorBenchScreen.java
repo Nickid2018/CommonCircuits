4270718ae9c2bfc37b1da17c91bac25dfc0216d5
Nickid2018 <nickid2018@outlook.com>
2023-02-17 15:00:14 +0800

    semiconductor bench completed

--- src/main/java/io/github/nickid2018/commoncircuits/client/gui/SemiconductorBenchScreen.java
+++ src/main/java/io/github/nickid2018/commoncircuits/client/gui/SemiconductorBenchScreen.java
@@ -31,8 +31,30 @@ public class SemiconductorBenchScreen extends AbstractContainerScreen<Semiconduc
             "gui.commoncircuits.semiconductor_bench.jfet", "gui.commoncircuits.semiconductor_bench.mosfet"
     };
 
+    public static final String[][][] CRAFT_BUTTON_TEXTS = new String[][][] {
+            {
+                {"item.commoncircuits.diode", "gui.commoncircuits.semiconductor_bench.diode.feature", "gui.commoncircuits.semiconductor_bench.diode.material"}
+            },
+            {
+                {"item.commoncircuits.npn_bjt", "gui.commoncircuits.semiconductor_bench.npn_bjt.feature", "gui.commoncircuits.semiconductor_bench.npn_bjt.material"},
+                {"item.commoncircuits.pnp_bjt", "gui.commoncircuits.semiconductor_bench.pnp_bjt.feature", "gui.commoncircuits.semiconductor_bench.pnp_bjt.material"}
+            },
+            {
+                {"item.commoncircuits.n_jfet", "gui.commoncircuits.semiconductor_bench.n_jfet.feature", "gui.commoncircuits.semiconductor_bench.n_jfet.material"},
+                {"item.commoncircuits.p_jfet", "gui.commoncircuits.semiconductor_bench.p_jfet.feature", "gui.commoncircuits.semiconductor_bench.p_jfet.material"}
+            },
+            {
+                {"item.commoncircuits.nmos", "gui.commoncircuits.semiconductor_bench.nmos.feature", "gui.commoncircuits.semiconductor_bench.nmos.material"},
+                {"item.commoncircuits.pmos", "gui.commoncircuits.semiconductor_bench.pmos.feature", "gui.commoncircuits.semiconductor_bench.pmos.material"}
+            }
+    };
+
     public static final int[][] CRAFT_BUTTON_TEX_POS = new int[][] {
             {72, 219}, {159, 219}
+    };
+
+    public static final int[][] CRAFT_BUTTON_POS = new int[][] {
+            {109, 23}, {109, 59}
     };
 
     public SemiconductorBenchScreen(SemiconductorBenchMenu menu, Inventory inventory) {
@@ -44,7 +66,7 @@ public class SemiconductorBenchScreen extends AbstractContainerScreen<Semiconduc
         imageWidth = 229;
         imageHeight = 218;
         inventoryLabelX = 35;
-        inventoryLabelY = 125;
+        inventoryLabelY = 126;
     }
 
     @Override
@@ -57,6 +79,10 @@ public class SemiconductorBenchScreen extends AbstractContainerScreen<Semiconduc
         //$$ RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         //$$ minecraft.getTextureManager().bind(TEXTURE);
         //#endif
+
+        int leftPos = this.leftPos;
+        int topPos = this.topPos;
+
         blit(poseStack, leftPos, topPos, 0, 0, imageWidth, imageHeight);
         int level = menu.getBlazeLevel();
         if (level > 0)
@@ -66,15 +92,40 @@ public class SemiconductorBenchScreen extends AbstractContainerScreen<Semiconduc
         int parsedY = my - topPos;
         int isIn = -1;
         for (int i = 0; i < 4; i++) {
+            boolean isInButton = parsedX >= BUTTONS_POS[i][0] && parsedX <= BUTTONS_POS[i][0] + 18 &&
+                    parsedY >= BUTTONS_POS[i][1] && parsedY <= BUTTONS_POS[i][1] + 18;
+            if (isInButton)
+                isIn = i;
             if ((menu.getMode() & 0b11) == i)
                 blit(poseStack, leftPos + BUTTONS_POS[i][0], topPos + BUTTONS_POS[i][1], BUTTONS_TEX_POS[i][1][0], BUTTONS_TEX_POS[i][1][1], 18, 18);
-            else if (parsedX >= BUTTONS_POS[i][0] && parsedX <= BUTTONS_POS[i][0] + 18 &&
-                    parsedY >= BUTTONS_POS[i][1] && parsedY <= BUTTONS_POS[i][1] + 18) {
+            else if (isInButton)
                 blit(poseStack, leftPos + BUTTONS_POS[i][0], topPos + BUTTONS_POS[i][1], BUTTONS_TEX_POS[i][2][0], BUTTONS_TEX_POS[i][2][1], 18, 18);
-                isIn = i;
-            } else
+            else
                 blit(poseStack, leftPos + BUTTONS_POS[i][0], topPos + BUTTONS_POS[i][1], BUTTONS_TEX_POS[i][0][0], BUTTONS_TEX_POS[i][0][1], 18, 18);
         }
+
+        int mode = menu.getMode();
+        int isInCraft = -1;
+        for (int i = 0; i < 2 && i <= mode; i++) {
+            boolean isInButton = parsedX >= CRAFT_BUTTON_POS[i][0] && parsedX <= CRAFT_BUTTON_POS[i][0] + 87 &&
+                    parsedY >= CRAFT_BUTTON_POS[i][1] && parsedY <= CRAFT_BUTTON_POS[i][1] + 36;
+            if (isInButton) {
+                isInCraft = i;
+                blit(poseStack, leftPos + CRAFT_BUTTON_POS[i][0], topPos + CRAFT_BUTTON_POS[i][1], CRAFT_BUTTON_TEX_POS[1][0], CRAFT_BUTTON_TEX_POS[1][1], 87, 36);
+            } else
+                blit(poseStack, leftPos + CRAFT_BUTTON_POS[i][0], topPos + CRAFT_BUTTON_POS[i][1], CRAFT_BUTTON_TEX_POS[0][0], CRAFT_BUTTON_TEX_POS[0][1], 87, 36);
+        }
+
+        for (int i = 0; i < 2 && i <= mode; i++) {
+            int height = font.lineHeight;
+            int y = topPos + CRAFT_BUTTON_POS[i][1] + 5;
+            int x = leftPos + CRAFT_BUTTON_POS[i][0] + 5;
+            font.draw(poseStack, CompatUtil.translated(CRAFT_BUTTON_TEXTS[mode][i][0]), x, y, 0x404040);
+            font.draw(poseStack, CompatUtil.translated(CRAFT_BUTTON_TEXTS[mode][i][2]), x, y + height, 0x404040);
+        }
+
+        if (isInCraft != -1)
+            renderTooltip(poseStack, CompatUtil.translated(CRAFT_BUTTON_TEXTS[mode][isInCraft][1]), mx, my);
 
         if (isIn != -1)
             renderTooltip(poseStack, CompatUtil.translated(BUTTONS_TEXT[isIn]), mx, my);
@@ -96,12 +147,19 @@ public class SemiconductorBenchScreen extends AbstractContainerScreen<Semiconduc
     @Override
     public boolean mouseClicked(double mx, double my, int i) {
         int isIn = -1;
-        mx -= leftPos;
-        my -= topPos;
+        double parsedX = mx - leftPos;
+        double parsedY = my - topPos;
         for (int index = 0; index < 4; index++) {
-            if (mx >= BUTTONS_POS[index][0] && mx <= BUTTONS_POS[index][0] + 18 &&
-                    my >= BUTTONS_POS[index][1] && my <= BUTTONS_POS[index][1] + 18) {
+            if (parsedX >= BUTTONS_POS[index][0] && parsedX <= BUTTONS_POS[index][0] + 18 &&
+                    parsedY >= BUTTONS_POS[index][1] && parsedY <= BUTTONS_POS[index][1] + 18) {
                 isIn = index;
+                break;
+            }
+        }
+        for (int index = 0; index < 2 && index <= menu.getMode(); index++) {
+            if (parsedX >= CRAFT_BUTTON_POS[index][0] && parsedX <= CRAFT_BUTTON_POS[index][0] + 87 &&
+                    parsedY >= CRAFT_BUTTON_POS[index][1] && parsedY <= CRAFT_BUTTON_POS[index][1] + 36) {
+                isIn = index + 4;
                 break;
             }
         }
--- src/main/java/io/github/nickid2018/commoncircuits/inventory/SemiconductorBenchMenu.java
+++ src/main/java/io/github/nickid2018/commoncircuits/inventory/SemiconductorBenchMenu.java
@@ -12,6 +12,7 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
@@ -19,6 +20,22 @@ public class SemiconductorBenchMenu extends AbstractContainerMenu {
 
     private final Container container;
     private final ContainerData containerData;
+
+    public static final int[][][] RECIPES = new int[][][] {
+            {{2, 1, 1, 1}},
+            {{3, 1, 1, 2}, {3, 1, 2, 1}},
+            {{3, 1, 2, 1}, {3, 1, 1, 2}},
+            {{4, 1, 2, 1}, {4, 1, 1, 2}}
+    };
+
+    public static final Item[][] RESULT = new Item[][] {
+            {CommonCircuitsItems.DIODE},
+            {CommonCircuitsItems.NPN_BJT, CommonCircuitsItems.PNP_BJT},
+            {CommonCircuitsItems.N_JFET, CommonCircuitsItems.P_JFET},
+            {CommonCircuitsItems.NMOS, CommonCircuitsItems.PMOS}
+    };
+
+    private Slot[] slotData = new Slot[6];
 
     public SemiconductorBenchMenu(int syncId, Inventory playerInventory) {
         this(syncId, playerInventory, new SimpleContainer(6), new SimpleContainerData(2));
@@ -31,12 +48,12 @@ public class SemiconductorBenchMenu extends AbstractContainerMenu {
         this.container = container;
         this.containerData = containerData;
         container.startOpen(inventory.player);
-        addSlot(new ConditionSlot(container, 0, 36, 23, itemStack -> CompatUtil.isSameItemTag(itemStack, CommonCircuitsTags.COPPER_DUSTS)));
-        addSlot(new ConditionSlot(container, 1, 36, 51, itemStack -> CompatUtil.isSameItem(itemStack, CommonCircuitsItems.BLACK_WAX)));
-        addSlot(new ConditionSlot(container, 2, 36, 79, itemStack -> CompatUtil.isSameItem(itemStack, CommonCircuitsItems.P_SEMICONDUCTOR)));
-        addSlot(new ConditionSlot(container, 3, 36, 107, itemStack -> CompatUtil.isSameItem(itemStack, CommonCircuitsItems.N_SEMICONDUCTOR)));
-        addSlot(new ConditionSlot(container, 4, 108, 107, itemStack -> CompatUtil.isSameItem(itemStack, Items.BLAZE_POWDER)));
-        addSlot(new ConditionSlot(container, 5, 180, 107, itemStack -> false));
+        addSlot(slotData[0] = new ConditionSlot(container, 0, 36, 23, itemStack -> CompatUtil.isSameItemTag(itemStack, CommonCircuitsTags.COPPER_DUSTS)));
+        addSlot(slotData[1] = new ConditionSlot(container, 1, 36, 51, itemStack -> CompatUtil.isSameItem(itemStack, CommonCircuitsItems.BLACK_WAX)));
+        addSlot(slotData[2] = new ConditionSlot(container, 2, 36, 79, itemStack -> CompatUtil.isSameItem(itemStack, CommonCircuitsItems.P_SEMICONDUCTOR)));
+        addSlot(slotData[3] = new ConditionSlot(container, 3, 36, 107, itemStack -> CompatUtil.isSameItem(itemStack, CommonCircuitsItems.N_SEMICONDUCTOR)));
+        addSlot(slotData[4] = new ConditionSlot(container, 4, 108, 107, itemStack -> CompatUtil.isSameItem(itemStack, Items.BLAZE_POWDER)));
+        addSlot(slotData[5] = new ConditionSlot(container, 5, 180, 107, itemStack -> false));
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 9; j++)
                 addSlot(new Slot(inventory, j + i * 9 + 9, 36 + j * 18, 137 + i * 18));
@@ -75,6 +92,23 @@ public class SemiconductorBenchMenu extends AbstractContainerMenu {
     public boolean clickMenuButton(Player player, int buttonID) {
         if (buttonID < 4) {
             containerData.set(0, buttonID);
+            return true;
+        } else if (buttonID < 6 && getBlazeLevel() > 0) {
+            int mode = getMode();
+            if (mode == 0 && buttonID == 5)
+                return false;
+            for (int i = 0; i < 4; i++)
+                if (slotData[i].getItem().getCount() - RECIPES[mode][buttonID - 4][i] < 0)
+                    return false;
+            if (slotData[5].hasItem() && !CompatUtil.isSameItem(slotData[5].getItem(), RESULT[mode][buttonID - 4]))
+                return false;
+            containerData.set(1, getBlazeLevel() - 1);
+            for (int i = 0; i < 4; i++)
+                slotData[i].getItem().shrink(RECIPES[mode][buttonID - 4][i]);
+            if (!slotData[5].hasItem())
+                slotData[5].set(new ItemStack(RESULT[mode][buttonID - 4]));
+            else
+                slotData[5].getItem().grow(1);
             return true;
         }
         return false;
