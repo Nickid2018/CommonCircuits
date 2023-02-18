d73863df37a8ef2f7ab389b26c27d785f41f8b6f
Nickid2018 <nickid2018@outlook.com>
2023-02-18 15:36:38 +0800

    *semiconductor bench texture

--- src/main/java/io/github/nickid2018/commoncircuits/block/SemiconductorBenchBlock.java
+++ src/main/java/io/github/nickid2018/commoncircuits/block/SemiconductorBenchBlock.java
@@ -2,17 +2,20 @@ package io.github.nickid2018.commoncircuits.block;
 
 import io.github.nickid2018.commoncircuits.block.entity.SemiconductorBenchBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
@@ -24,8 +27,23 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 
 public class SemiconductorBenchBlock extends BaseEntityBlock {
 
+    public static final EnumProperty<Direction> FACING = HorizontalDirectionalBlock.FACING;
+
     protected SemiconductorBenchBlock(Properties properties) {
         super(properties);
+        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
+        Direction direction = blockPlaceContext.getHorizontalDirection();
+        return defaultBlockState().setValue(FACING, direction.getOpposite());
     }
 
     @Nullable
@@ -65,6 +83,16 @@ public class SemiconductorBenchBlock extends BaseEntityBlock {
             }
             super.onRemove(blockState, level, blockPos, blockState2, bl);
         }
+    }
+
+    @Override
+    public BlockState rotate(BlockState blockState, Rotation rotation) {
+        return blockState.setValue(FACING, rotation.rotate(blockState.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState blockState, Mirror mirror) {
+        return blockState.rotate(mirror.getRotation(blockState.getValue(FACING)));
     }
 
     //#if MC>=11701
