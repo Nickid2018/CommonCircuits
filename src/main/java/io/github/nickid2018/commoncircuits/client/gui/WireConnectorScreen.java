e3e363ed0984eb6ce085b3b6d225c3e4700f0887
Nickid2018 <nickid2018@outlook.com>
2023-04-25 20:35:05 +0800

    update->1.19.4

--- src/main/java/io/github/nickid2018/commoncircuits/client/gui/WireConnectorScreen.java
+++ src/main/java/io/github/nickid2018/commoncircuits/client/gui/WireConnectorScreen.java
@@ -14,7 +14,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.StonecutterMenu;
 
 import java.util.List;
 
